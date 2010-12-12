db6af3bd55096cf36ecf2097c889bf1288d432c4
Andreas Lüthi <andreas.luethi@gmx.net>
2010-12-12 17:15:31 +0100

    Test if GIT commit works

--- src/ch/ubx/startlist/client/DateBox2.java
+++ src/ch/ubx/startlist/client/DateBox2.java
@@ -3,6 +3,8 @@ package ch.ubx.startlist.client;
 import com.google.gwt.user.client.ui.FocusWidget;
 import com.google.gwt.user.datepicker.client.DateBox;
 
+// XX
+
 public class DateBox2 extends DateBox {
 	public DateBox2() {
 		super();
